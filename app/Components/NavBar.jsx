da8772dd25bc06951e40c27a9c374a44dcbb70b9
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-02 23:30:02 +0530

    Add_Nav_Bar.jsx

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { assets } from "@/Assets/assets";
 
 const NavBar = () => {
+
+    const sideMenuRef = useRef();
+
+    const openMenu=()=>{
+        sideMenuRef.current.style.transform='translatex(-16rem)'
+    }
+    const closeMenu=()=>{
+        sideMenuRef.current.style.transform='translatex(16rem)'
+    }
   return (
     <>
-      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt="" className='w-full' />
+    </div>
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
         <a href="#top">
-            <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt="" />
+          <Image
+            src={assets.logo}
+            className="w-28 cursor-pointer mr-14"
+            alt=""
+          />
         </a>
-     <ul className="hidden md:flex items-center gap-6 lg:gap-8  rounded-full px-12 py-3">
-         <li><a href="#home">Home</a></li>
-         <li><a href="#about">About</a></li>
-         <li><a href="#skills">Skills</a></li>
-         <li><a href="#projects">Projects</a></li>
-         <li><a href="#contact">Contact</a></li>
-</ul>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8  rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+          <li>
+            <a  className="font-Ovo" href="#home">Home</a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about">About</a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#skills">Skills</a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#projects">Projects</a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact">Contact</a>
+          </li>
+        </ul>
 
+        <div className="flex items-center gap-4">
+           
+           <button>
+            <Image src={assets.moon_icon} alt="" className="w-6"/>
+           </button>
 
-        <div>
-            <a href="#contact"  className="hidden lg:flex item-center gap-3 px-10 py-2.5 border-gray-500 rounded-full ml-4">Contact<Image src={assets.arrow_icon} className='w-3' alt=""/></a>
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
+          >
+            Contact
+            <Image src={assets.arrow_icon} className="w-3" alt="" />
+          </a>
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image src={assets.menu_black} alt="" className="w-6"/>
+          </button>
         </div>
+
+        {/*----------* Mobile Menu*----------*/}
+        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+            <div className="absolute right-6 top-6" onClick={closeMenu}>
+                <Image src={assets.close_black}alt='' className="w-5 cursor-pointer " />
+            </div>
+            <li>
+            <a  className="font-Ovo" onClick={closeMenu} href="#home">Home</a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#about">About</a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#skills">Skills</a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#projects">Projects</a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu}href="#contact">Contact</a>
+          </li>
+        </ul>
       </nav>
     </>
   );
