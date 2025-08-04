1cd5ba61c1bb14bee9127b34437e6ad7013e2205
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-04 16:41:27 +0530

    change_layout_page

--- app/layout.js
+++ app/layout.js
@@ -21,8 +21,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${outfit.variable} ${ovo.variable} antialiased leading-8 overflow-x-hidden`}>
-      <body className="antialiased">
+    <html lang="en" className="scroll-smooth">
+      <body className={`${outfit.variable} ${ovo.variable} antialiased leading-8 overflow-x-hidden`}>
         {children}
       </body>
     </html>
