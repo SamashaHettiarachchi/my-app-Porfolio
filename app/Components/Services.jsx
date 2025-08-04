ce995a13b8c696a6ea0fccb36d5e8b218651d4ed
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-04 16:40:35 +0530

    create_services_part

---
+++ app/Components/Services.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { serviceData, assets } from "@/Assets/assets";
+import Image from "next/image";
+
+const Services = () => {
+  return (
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-Ovo">My Services</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        I offer a range of services to help you build and enhance your web presence. From responsive web design to front-end development using modern frameworks, I am dedicated to delivering high-quality, user-friendly solutions tailored to your needs. Whether you need a new website, an update to an existing one, or assistance with specific features, I am here to help bring your vision to life. Let's work together to create something amazing!
+      </p>
+      <div className='grid grid-cols-auto gap-6 my-10'>
+          {serviceData.map(({icon,title,description,link},index)=>(
+            <div key={index} className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500">
+              <Image src={icon} alt='' className='w-10'/>
+              <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+              <p className='text-sm text-gray-600 leading-5'>
+                {description}
+              </p>
+              <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read more
+                <Image src={assets.right_arrow} alt='arrow' className='w-4' />
+              </a>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default Services
