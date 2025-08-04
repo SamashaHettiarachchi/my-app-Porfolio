0b5bf11bb7d495e6ed17b78d31336cb7627baabd
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-04 16:41:39 +0530

    change_page

--- app/page.js
+++ app/page.js
@@ -2,13 +2,19 @@
 import NavBar from "./Components/NavBar";
 import Header from "./Components/Header";
 import About from "./Components/About";
+import Services from "./Components/Services";
+import Work from "./Components/work";
+import Contact from "./Components/Contact";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Header />
-      <About/>
+      <About />
+      <Services />
+      <Work />
+      <Contact />
     </>
   );
 }
