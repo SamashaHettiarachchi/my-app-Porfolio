c3332bb65a7636dbb28c9e724ecca02dcd738135
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-04 16:40:48 +0530

    create_navBar_part

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -1,23 +1,44 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { assets } from "@/Assets/assets";
 
 const NavBar = () => {
+  const [isScroll, setIsScroll] = useState(false);
+  const sideMenuRef = useRef();
 
-    const sideMenuRef = useRef();
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translatex(-16rem)";
+  };
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translatex(16rem)";
+  };
 
-    const openMenu=()=>{
-        sideMenuRef.current.style.transform='translatex(-16rem)'
-    }
-    const closeMenu=()=>{
-        sideMenuRef.current.style.transform='translatex(16rem)'
-    }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
-        <Image src={assets.header_bg_color} alt="" className='w-full' />
-    </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : " "
+        }`}
+      >
         <a href="#top">
           <Image
             src={assets.logo}
@@ -27,27 +48,36 @@ const NavBar = () => {
         </a>
         <ul className="hidden md:flex items-center gap-6 lg:gap-8  rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
           <li>
-            <a  className="font-Ovo" href="#home">Home</a>
+            <a className="font-Ovo" href="#home">
+              Home
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#about">About</a>
+            <a className="font-Ovo" href="#about">
+              About
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#skills">Skills</a>
+            <a className="font-Ovo" href="#skills">
+              Skills
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#projects">Projects</a>
+            <a className="font-Ovo" href="#projects">
+              Projects
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#contact">Contact</a>
+            <a className="font-Ovo" href="#contact">
+              Contact
+            </a>
           </li>
         </ul>
 
         <div className="flex items-center gap-4">
-           
-           <button>
-            <Image src={assets.moon_icon} alt="" className="w-6"/>
-           </button>
+          <button>
+            <Image src={assets.moon_icon} alt="" className="w-6" />
+          </button>
 
           <a
             href="#contact"
@@ -57,29 +87,46 @@ const NavBar = () => {
             <Image src={assets.arrow_icon} className="w-3" alt="" />
           </a>
           <button className="block md:hidden ml-3" onClick={openMenu}>
-            <Image src={assets.menu_black} alt="" className="w-6"/>
+            <Image src={assets.menu_black} alt="" className="w-6" />
           </button>
         </div>
 
         {/*----------* Mobile Menu*----------*/}
-        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
-            <div className="absolute right-6 top-6" onClick={closeMenu}>
-                <Image src={assets.close_black}alt='' className="w-5 cursor-pointer " />
-            </div>
-            <li>
-            <a  className="font-Ovo" onClick={closeMenu} href="#home">Home</a>
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer "
+            />
+          </div>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#home">
+              Home
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#about">About</a>
+            <a className="font-Ovo" onClick={closeMenu} href="#about">
+              About
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#skills">Skills</a>
+            <a className="font-Ovo" onClick={closeMenu} href="#skills">
+              Skills
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#projects">Projects</a>
+            <a className="font-Ovo" onClick={closeMenu} href="#projects">
+              Projects
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu}href="#contact">Contact</a>
+            <a className="font-Ovo" onClick={closeMenu} href="#contact">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
