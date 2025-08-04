84bc25752a4007eb06cbca81fa69804d56823477
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-04 23:07:47 +0530

    Add_Dark_mode_components

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className=" w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+    <div
+      id="top"
+      className=" w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4"
+    >
       <div>
         <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
       </div>
