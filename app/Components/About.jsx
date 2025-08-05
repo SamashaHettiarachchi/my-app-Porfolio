cb037c244bd98eb882deb7caa0a0b411a46494ca
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-05 10:24:39 +0530

    create_DarkMode

--- app/Components/About.jsx
+++ app/Components/About.jsx
@@ -1,12 +1,16 @@
-import { assets, infoList ,toolsData} from "@/Assets/assets";
+import { assets, infoList, toolsData } from "@/Assets/assets";
 import React from "react";
 import Image from "next/image";
 
-const About = () => {
+const About = ({ isdarkMode }) => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
-      <h2 className="text-center text-5xl font-Ovo">About me</h2>
+      <h4 className="text-center mb-2 text-lg font-Ovo dark:text-white">
+        Introduction
+      </h4>
+      <h2 className="text-center text-5xl font-Ovo dark:text-white">
+        About me
+      </h2>
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
         <div className="w-64 sm:w-80 rounded-3xl max-w-none">
           <Image
@@ -16,7 +20,7 @@ const About = () => {
           />
         </div>
         <div className="flex-1">
-          <p className="mb-10 max-w-2xl font-Ovo">
+          <p className="mb-10 max-w-2xl font-Ovo dark:text-white/90">
             I'm a passionate Front-End Developer with a strong focus on building
             responsive, user-friendly web interfaces. I specialize in modern
             JavaScript frameworks like React and Vue, and have experience
@@ -28,21 +32,35 @@ const About = () => {
             {infoList.map(({ icon, iconDark, title, description }, index) => (
               <li
                 key={index}
-                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50"
               >
-                <Image src={icon} alt={title} className="w-7 mt-3" />
-                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
-                <p className="text-gray-600 text-sm">{description}</p>
+                <Image
+                  src={isdarkMode ? iconDark : icon}
+                  alt={title}
+                  className="w-7 mt-3"
+                />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {title}
+                </h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">
+                  {description}
+                </p>
               </li>
             ))}
           </ul>
-          <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
-          <ul className="flex items-center gap-3 sm:gap-5">{toolsData.map((tool,index)=>(
-            <li className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
-            key={index}>
-                <Image src={tool} alt='Tool' className="w-5 sm:w-7"/>
-            </li>
-          ))}</ul>
+          <h4 className="my-6 text-gray-700 font-Ovo dark:text-white">
+            Tools I use
+          </h4>
+          <ul className="flex items-center gap-3 sm:gap-5">
+            {toolsData.map((tool, index) => (
+              <li
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 dark:border-white/50"
+                key={index}
+              >
+                <Image src={tool} alt="Tool" className="w-5 sm:w-7" />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
