0755eb11a37aada84b623603d06d7682ffa10c24
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-05 10:23:52 +0530

    create_darkMode

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = ({ isdarkMode, setIsDarkMode }) => {
       <nav
         className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
           isScroll
-            ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20"
+            ? "bg-white/80 backdrop-blur-lg shadow-sm dark:bg-darkTheme/80 dark:shadow-white/20"
             : ""
         }`}
       >
@@ -48,29 +48,35 @@ const NavBar = ({ isdarkMode, setIsDarkMode }) => {
             alt=""
           />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8  rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            isScroll
+              ? "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-darkTheme/50"
+              : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"
+          }`}
+        >
           <li>
-            <a className="font-Ovo" href="#top">
+            <a className="font-Ovo dark:text-white" href="#top">
               Home
             </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#about">
+            <a className="font-Ovo dark:text-white" href="#about">
               About
             </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#services">
+            <a className="font-Ovo dark:text-white" href="#services">
               Services
             </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#work">
+            <a className="font-Ovo dark:text-white" href="#work">
               Work
             </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#contact">
+            <a className="font-Ovo dark:text-white" href="#contact">
               Contact
             </a>
           </li>
@@ -90,54 +96,82 @@ const NavBar = ({ isdarkMode, setIsDarkMode }) => {
 
           <a
             href="#contact"
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50 dark:text-white"
           >
             Contact
-            <Image src={assets.arrow_icon} className="w-3" alt="" />
+            <Image
+              src={isdarkMode ? assets.arrow_icon_dark : assets.arrow_icon}
+              className="w-3"
+              alt=""
+            />
           </a>
           <button
             className="block md:hidden ml-3"
             onClick={openMenu}
             suppressHydrationWarning={true}
           >
-            <Image src={assets.menu_black} alt="" className="w-6" />
+            <Image
+              src={isdarkMode ? assets.menu_white : assets.menu_black}
+              alt=""
+              className="w-6"
+            />
           </button>
         </div>
 
         {/*----------* Mobile Menu*----------*/}
         <ul
           ref={sideMenuRef}
-          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 dark:bg-darkHover dark:text-white transition duration-500"
         >
           <div className="absolute right-6 top-6" onClick={closeMenu}>
             <Image
-              src={assets.close_black}
+              src={isdarkMode ? assets.close_white : assets.close_black}
               alt=""
-              className="w-5 cursor-pointer "
+              className="w-5 cursor-pointer"
             />
           </div>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#top">
+            <a
+              className="font-Ovo dark:text-white"
+              onClick={closeMenu}
+              href="#top"
+            >
               Home
             </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#about">
+            <a
+              className="font-Ovo dark:text-white"
+              onClick={closeMenu}
+              href="#about"
+            >
               About
             </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#services">
+            <a
+              className="font-Ovo dark:text-white"
+              onClick={closeMenu}
+              href="#services"
+            >
               Services
             </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#work">
+            <a
+              className="font-Ovo dark:text-white"
+              onClick={closeMenu}
+              href="#work"
+            >
               Work
             </a>
           </li>
           <li>
-            <a className="font-Ovo" onClick={closeMenu} href="#contact">
+            <a
+              className="font-Ovo dark:text-white"
+              onClick={closeMenu}
+              href="#contact"
+            >
               Contact
             </a>
           </li>
